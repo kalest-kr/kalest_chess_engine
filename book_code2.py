ed9deb323a53dabba9b436b09daec51d19bd979f
kalest <ginokim55@gmail.com>
2026-03-05 09:31:06 +0900

    commit_3

--- book_code2.py
+++ book_code2.py
@@ -226,3 +226,54 @@ plt.plot(avg_rates1, color='red', label='Agent')
 plt.plot(avg_rates2, color='blue', label='Alpha Agent')
 plt.legend()
 plt.show()
+
+class GridWorld:
+    def __init__(self):
+        self.action_space = [0, 1, 2, 3]
+        self.acting_meaning = {
+            0:'up', 1:'down', 2:'right', 3:'left'
+        }
+        self.reward_map = np.array(
+            [[0, 0, 0, 1],
+             [0, None, 0, -1],
+             [0, 0, 0, 0]]
+        )
+        self.goal_state = (0, 3)
+        self.wall_state = (1, 1)
+        self.start_state = (2, 0)
+        self.agent_state = self.start_state
+
+        @property
+        def height(self):
+            return len(self.reward_map)
+        @property
+        def width(self):
+            return len(self.reward_map[0])
+        @property
+        def shape(self):
+            return self.reward_map.shape
+
+        def actions(self):
+            return self.action_space
+
+        def states(self):
+            for h in range(self.height):
+                for w in range(self.width):
+                    yield (h, w)
+
+        def next_state(self, state, action):
+            action_move_map = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            move = action_move_map[action]
+            next_state = (state[0] + move[0], state[1] + move[1])
+            ny, nx = next_state
+
+            if nx < 0 or nx >= self.width or ny < 0 or ny >= self.height:
+                next_state = state
+            elif next_state == self.wall_state:
+                next_state = state
+
+            return next_state
+
+        def reward(self, state, action, next_state):
+            return self.reward_map[next_state]
+
